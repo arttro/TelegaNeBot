e3481978dd6eb9b1e165199373ea6e0fbd68ca22
arttro <40605997+arttro@users.noreply.github.com>
2020-12-23 15:58:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using System.Resources;
+
+namespace weather
+{
+    class Program
+    {
+        private static TelegramBotClient client;
+
+        static void Main(string[] args)
+        {
+
+            string token = "1318724810:AAEFZish3unm5KBLkwlfepuuYTPFZZkgZFQ";
+
+            client = new TelegramBotClient(token);
+            client.OnMessage += BotOnMessageReceived;
+            client.OnMessageEdited += BotOnMessageReceived;
+            client.StartReceiving();
+
+            Console.ReadLine();
+
+            client.StopReceiving();
+        }
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            string key = "29f2f4edc2ad34bf18d8a6c2f640dc45";
+
+            try
+            {
+                
+                string url = "http://api.openweathermap.org/data/2.5/weather?q=" + message.Text + "&units=metric&appid=" + key;
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+                string result = streamReader.ReadToEnd();
+                WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(result);
+
+                if (message?.Type == MessageType.Text)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "Temperature in " + weatherResponse.Name + ": " + weatherResponse.Main.Temp + "°C \n");
+                }
+            }
+            catch {
+                if (message?.Type == MessageType.Text)
+                {
+                    await client.SendTextMessageAsync(message.Chat.Id, "City not found");
+                }
+            }
+
+            
+        }
+        
+    }
+}
---
+++ TemperatureInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace weather
+{
+    class TemperatureInfo
+    {
+        public float Temp { get; set; }
+    }
+}
